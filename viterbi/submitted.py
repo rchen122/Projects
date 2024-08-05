cb791f0284e2ef634c8071b164e1dd8388b07f79
rchen122 <120858799+rchen122@users.noreply.github.com>
2024-08-05 11:36:29 -0400

    Update submitted.py

--- viterbi/submitted.py
+++ viterbi/submitted.py
@@ -153,15 +153,7 @@ def viterbi(test, train):
 	return test_tagged 
 
 
-def viterbi_ec(test, train):
-    '''
-    Implementation for the improved viterbi tagger.
-    input:  test data (list of sentences, no tags on the words). E.g.,  [[word1, word2], [word3, word4]]
-            training data (list of sentences, with tags on the words). E.g.,  [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
-    output: list of sentences, each sentence is a list of (word,tag) pairs.
-            E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
-    '''
-    raise NotImplementedError("You need to write this part!")
+
 
 
 
